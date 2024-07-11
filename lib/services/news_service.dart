4cd66a8883929bf92073cc72d2eff08fe9be6caa
Yoga Syahputra <94789352+Yoga-Syahputra@users.noreply.github.com>
2024-07-11 11:24:35 +0700

    Update news_service.dart

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -8,7 +8,7 @@ class NewsService extends ChangeNotifier {
   List get articles => _articles;
 
   Future<void> fetchNews() async {
-    final response = await http.get(Uri.parse('https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=YOUR API KEY'));
+    final response = await http.get(Uri.parse('https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=YOUR_API_KEY'));
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       _articles = data['articles'] ?? [];
